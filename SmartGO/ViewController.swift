4bb23e2f5787161dff6744a9e890b173b616fecb
thtuan <tuan.ho@gcalls.co>
2017-06-23 16:23:53 +0700

    Initial Commit

---
+++ SmartGO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartGO
+//
+//  Created by thanh tuan on 6/23/17.
+//  Copyright © 2017 thanh tuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
